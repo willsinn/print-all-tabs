4c19be8e18842e9f298b18d09a28629c3c797d35
willsinn <30275717+willsinn@users.noreply.github.com>
2018-02-09 23:27:41 -0500

    Highlighted Tabs

---
+++ background.js
@@ -0,0 +1,40 @@
+/*
+printAllTabsApp
+----------------------------------------------------
+Tab selection function array format
+   1. add event tab selector
+   2. add event tab deselector
+   3. add event select all tabs
+   4. add event deselect all tabs
+
+Print all selected function
+   1. add event on print, locate all selected tabs
+   2. convert all content into one format OR limit to just viewed PDFs
+   3. append PDF's ordered by selection
+   4. push new file to printer
+
+Cancel print function
+   1. return previously selected tabs
+*/
+
+
+/* Tab selector
+    1. add onClick event listener
+*/
+
+var selectedTabs = {tab:}
+
+  chrome.browserAction.onClick.addListener(function(tab) {
+          chrome.tabs.query({currentWindow, highlighted: true}, function(tabs) {
+            var selected = tabs.filter(function(tab) {
+              return tab.highlighted;
+            })[0];
+
+        })
+    })
+  })
+
+  chrome.tabs.onHighlighted.addListener(function callback) //if else statement using highlighted boolean to determine if adding or removing tab
+
+
+  .forEach(function())   //iterates over array, use to print selected tabs
